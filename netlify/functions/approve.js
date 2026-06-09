0a06b05c72ef5294815be0451eedf0186c68b8ce
tanahin <batikperca.jogja@gmail.com>
2026-06-09 17:12:13 +0700

    Update approve.js

--- netlify/functions/approve.js
+++ netlify/functions/approve.js
@@ -1,1 +1,12 @@
 
+const axios = require('axios');
+exports.handler = async (event) => {
+  const { paymentId } = JSON.parse(event.body);
+  const apiKey = process.env.PI_API_KEY;
+  try {
+    await axios.post(`https://api.minepi.com/v2/payments/${paymentId}/approve`, {}, { headers: { 'Authorization': `Key ${apiKey}` } });
+    return { statusCode: 200, body: JSON.stringify({ message: 'Approved' }) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+};
