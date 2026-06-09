e53d831647b4667d4358ea0d033162fa92d4585a
tanahin <batikperca.jogja@gmail.com>
2026-06-09 17:14:00 +0700

    Update cancel.js

--- netlify/functions/cancel.js
+++ netlify/functions/cancel.js
@@ -1,1 +1,11 @@
-
+const axios = require('axios');
+exports.handler = async (event) => {
+  const { paymentId } = JSON.parse(event.body);
+  const apiKey = process.env.PI_API_KEY;
+  try {
+    await axios.post(`https://api.minepi.com/v2/payments/${paymentId}/cancel`, {}, { headers: { 'Authorization': `Key ${apiKey}` } });
+    return { statusCode: 200, body: JSON.stringify({ message: 'Cancelled' }) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+};
